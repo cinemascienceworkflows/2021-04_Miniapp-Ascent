84b45148b3a9426550f50ca59c18ce0c900e56bc
David Rogers <dhr@lanl.gov>
2021-05-13 12:40:45 -0400

    updating validate

--- validate/validate.sh
+++ validate/validate.sh
@@ -6,22 +6,18 @@ echo "----------------------------------------------------------------------"
 echo "PTN: validating" 
 
 OUTPUT=$PANTHEON_RUN_DIR/cinema_databases/$PANTHEON_CDB
-GOLD=validate/data/cloverleaf3d_001.cdb
 
 echo "     $OUTPUT"
 
-dirs="0.0 2.0 5.0"
-img="0.0_0.0_pantheon.cdb.png"
+images="000010.npz 000100.npz"
 
 PASS=true
 if [ -d $OUTPUT ]; then
-    for val in $dirs; do
-        if cmp "$OUTPUT/$val/$img" "$GOLD/$val/$img"; then
-            echo "     Comparing images $GOLD/$val/$img"
+    for val in $images; do
+        echo "     $OUTPUT/$val"
+        if [ -f $OUTPUT/$val ]; then
+            PASS=true
         else
-            echo "FILES differ:"
-            echo "    $OUTPUT"
-            echo "    $GOLD"
             PASS=false
         fi
     done
