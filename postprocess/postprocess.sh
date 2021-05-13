a931733016431e5231e40d1240c61b2865380bf9
David Rogers <dhr@lanl.gov>
2021-05-13 10:38:09 -0400

    adding conversion

--- postprocess/postprocess.sh
+++ postprocess/postprocess.sh
@@ -6,13 +6,26 @@ echo "----------------------------------------------------------------------"
 echo "PTN: Post-processing" 
 echo "     packaging up cinema database to:" 
 
+# make the cinema data directory 
+mkdir $PANTHEON_RUN_DIR/cinema_databases
+
 # install cinema viewers
 cp -rf inputs/cinema/* $PANTHEON_RUN_DIR/cinema_databases
     # redirect the python notebook viewer to the database
 sed -i "s#CINEMA_DB_PATH#$PANTHEON_CDB#" $PANTHEON_RUN_DIR/cinema_databases/cinema.ipynb 
 
 pushd $PANTHEON_RUN_DIR > /dev/null 2>&1
+pushd cinema_databases  > /dev/null 2>&1
 
+# convert the output from ascent HDF5 to CIS
+module load python/3.7.0-anaconda3-5.3.0
+./cisconvert --verbose --input ../ --output pantheon.cdb
+    # clean up
+rm -rf cinemasci
+rm -f  cisconvert
+popd > /dev/null 2>&1
+
+# bundle up the results
 TARNAME=cinema_databases
 echo "     $PANTHEON_DATA_DIR/${TARNAME}.tar.gz" 
 echo "----------------------------------------------------------------------"
