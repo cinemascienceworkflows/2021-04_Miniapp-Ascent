7fb12fa1599b728a32fabb683a5fa5906aa7edf6
Ethan Stam <ethan.stam@gmail.com>
2021-06-21 13:32:45 -0600

    sed command changed for linux os

--- travis/run.sh
+++ travis/run.sh
@@ -8,10 +8,10 @@ cd ${TRAVIS_BUILD_DIR}
 
 # set PANTHOEN_BASE_PATH to travis directory and mkdir that directory
 mkdir -p ${HOME}/pantheon_work
-sed -i '' "s/PANTHEON_BASE_PATH=WarningNotSet/PANTHEON_BASE_PATH=\${HOME}\/pantheon_work/g" ./bootstrap.env
+sed -i "s/PANTHEON_BASE_PATH=WarningNotSet/PANTHEON_BASE_PATH=\${HOME}\/pantheon_work/g" ./bootstrap.env
 
 # placeholder set compute allocation to NONE, will not be used on travis
-sed -i '' "s/COMPUTE_ALLOCATION=WarningNotSet/COMPUTE_ALLOCATION=NONE/g" ./bootstrap.env
+sed -i "s/COMPUTE_ALLOCATION=WarningNotSet/COMPUTE_ALLOCATION=NONE/g" ./bootstrap.env
 
 # execute
 ./execute
